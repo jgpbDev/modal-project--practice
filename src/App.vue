7f034558b74b6469bb4157d283f83ecba0f799e7
jgpbDev <jorgeg.perezb@gmail.com>
2022-04-19 13:51:58 -0500

    Generating modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="sale"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue"
 
 export default {
+  //It is optional to name our components
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'This is my title in the data',
+      header: 'Sign up to this new app',
+      text: 'This is the text that I am using for testing Vue 3'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus();
+    }
   }
 }
 </script>
@@ -22,5 +35,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #dddddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="backdrop">
+    <!--
+      This class with v-bind is a dynamic class, the prop
+      name is the class and its value is if it is going to be
+      applied or not 
+
+      This could be useful if we want to apply themes or custom
+      options in our app
+    -->
+    <div class="modal" :class="{ sale: theme === 'sale'}">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'text', 'theme'],
+}
+</script>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  h1 {
+    color: aqua;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+  }
+
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+</style>
